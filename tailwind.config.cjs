db9e42139dfb86525a8803a73d891d716bb4fb7b
neel-kumar-v <greenworldwins@gmail.com>
2023-06-19 00:56:11 -0400

    fix tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      'brown': '#ae7541',
-      'darkbrown': '#76502c',
-      'lightbrown': '#76502c',
-      'lightgray': '#dddddd',
-      'darkgray': '#2c2c2c',
-
+    extend: {
+      colors: {
+        'brown': '#ae7541',
+        'darkbrown': '#76502c',
+        'lightbrown': '#76502c',
+        'lightgray': '#dddddd',
+        'darkgray': '#2c2c2c',
+  
+      },
+      fontFamily: {
+        fontA: ['Karla', 'sans-serif'],
+      },
     },
-    fontFamily: {
-      fontA: ['Karla', 'sans-serif'],
-      fontB: []
-    },
-    
-    extend: {},
   },
   plugins: [],
 };
