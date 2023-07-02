929d06830cde0dc9076d6a49643a90e715d470b6
neel-kumar-v <79852376+neel-kumar-v@users.noreply.github.com>
2023-07-02 20:28:52 +0000

    format slug page + standardize typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    colors: {
-      'brown': '#ae7541',
-      'darkbrown': '#76502c',
-      'lightbrown': '#76502c',
-      'lightgray': '#dddddd',
-      'darkgray': '#2c2c2c',
+  // theme: {
+  //   colors: {
+  //     'brown': '#ae7541',
+  //     'darkbrown': '#76502c',
+  //     'lightbrown': '#76502c',
+  //     'lightgray': '#dddddd',
+  //     'darkgray': '#2c2c2c',
 
-    },
-    fontFamily: {
-      fontA: ['Karla', 'sans-serif'],
-      fontB: []
-    },
+  //   },
+  //   fontFamily: {
+  //     fontA: ['Karla', 'sans-serif'],
+  //     fontB: []
+  //   },
     
-    extend: {},
-  },
+  //   extend: {},
+  // },
   plugins: [],
 };
