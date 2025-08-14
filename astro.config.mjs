0f27f4cfe5ffb934d1948d19b1b600785f6286b1
neel-kumar-v <greenworldwins@gmail.com>
2025-08-13 20:44:33 -0400

    Update Astro config and dependencies
    Switched Astro adapter from '@astrojs/vercel/static' to '@astrojs/vercel' in astro.config.mjs and updated related configuration. Upgraded Astro, Tailwind, Partytown, Vercel, and other dependencies to latest versions in package.json and package-lock.json for improved compatibility and features.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
+// import vercelStatic from "@astrojs/vercel";
 
 import partytown from "@astrojs/partytown";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
+  // output: "static",
+  // adapter: vercelStatic(),
   integrations: [tailwind(), partytown()],
+
   output: "static",
-  adapter: vercelStatic(),
+  adapter: vercel(),
 });
